08c5f1bafc6b875eebd58d088a201d36444a5d82
mcfrojd <info@mcfrojd.com>
2019-04-18 12:27:12 +0200

    Updated Custom header

--- www/github/maykar/compact-custom-header.js
+++ www/github/maykar/compact-custom-header.js
@@ -1,4 +1,4 @@
-import "./compact-custom-header-editor.js?v=1.0.3b0";
+import "./compact-custom-header-editor.js?v=1.0.3b1";
 
 export const LitElement = Object.getPrototypeOf(
   customElements.get("ha-panel-lovelace")
@@ -312,8 +312,10 @@ if (!customElements.get("compact-custom-header")) {
           }
         }
       }
-      tabContainer.style.marginRight = marginRight + "px";
-      tabContainer.style.marginLeft = marginLeft + "px";
+      if (tabContainer) {
+        tabContainer.style.marginRight = marginRight + "px";
+        tabContainer.style.marginLeft = marginLeft + "px";
+      }
     }
 
     get rootElement() {
@@ -440,7 +442,8 @@ if (!customElements.get("compact-custom-header")) {
       if (
         !root.querySelector('[id="cch_iron_selected"]') &&
         !this.editMode &&
-        !conditionalTabs
+        !conditionalTabs &&
+        tabContainer
       ) {
         let style = document.createElement("style");
         style.setAttribute("id", "cch_iron_selected");
@@ -747,7 +750,7 @@ if (!customElements.get("compact-custom-header")) {
       if (this.prevColor == undefined) this.prevColor = {};
       if (this.prevState == undefined) this.prevState = [];
       const conditional_styles = this.cchConfig.conditional_styles;
-      let tabContainer = tabs[0].parentNode;
+      let tabContainer = tabs[0] ? tabs[0].parentNode : "";
       let element, color, background, hide, onIcon, offIcon, iconElement;
 
       const styleElements = (
